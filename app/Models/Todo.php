eb09b93a9efdc6f1375f5358047b7e5072f0f021
Izalhadji31 <ryzalhadji@gmail.com>
2025-03-15 14:43:19 +0700

    menambahkan properti fillable ke dalam model todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'user_id',
+        'is_complete',
+    ];
 }
