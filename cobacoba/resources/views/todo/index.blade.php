d716f5482950bf1db19a90ac861c80ae8669b696
Izalhadji31 <ryzalhadji@gmail.com>
2025-03-15 14:59:32 +0700

    menambahkan slot header dengan judul "Todo"

---
+++ cobacoba/resources/views/todo/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-sembold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Todo') }}
+        </h2>
+    </x-slot>
+
+    
+</x-app-layout>
